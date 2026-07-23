afb37c9aa9cb0bed7ab61b2408a1bf71e6f58a18
Marius Stokkebro <masto@itu.dk>
2026-07-23 17:26:05 +0200

    basic wallrunning

---
+++ Scripts/aim_look.gd
@@ -0,0 +1,73 @@
+extends Node
+
+#nodes
+@export_group("Nodes")
+#Character node
+@export var character: CharacterBody3D
+#Head node
+@export var head: Node3D
+#Settings
+@export_group("settings")
+@export_subgroup("mouse settings")
+@export_range (1,100,1) var mouse_sensitivity: int = 50
+@export_subgroup("clamp settings")
+@export var max_pitch: float = 89
+@export var min_pitch: float = -89
+
+
+func _ready() -> void:
+	Input.set_use_accumulated_input(false)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventKey:
+			if event.is_action_pressed("ui_cancel"):
+				get_tree().quit()
+	
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
+		return
+		
+	if event is InputEventKey:
+		if event.is_action_pressed("ui_cancel"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		return
+		
+	if event is InputEventMouseMotion:
+		aim_look(event)
+		
+		
+func aim_look(event: InputEventMouseMotion)->void:
+	var viewport_transform: Transform2D = get_tree().root.get_final_transform()
+	var motion: Vector2 = event.xformed_by(viewport_transform).relative
+	var degrees_per_unit: float = 0.001
+	
+	motion *= mouse_sensitivity
+	motion *= degrees_per_unit
+	
+	add_yaw(motion.x)
+	add_pitch(motion.y)
+	clamp_pitch()
+	
+	
+func add_yaw(amount)->void:
+	if is_zero_approx(amount):
+		return
+		
+	character.rotate_object_local(Vector3.DOWN,deg_to_rad(amount))
+	character.orthonormalize()
+	
+func add_pitch(amount)->void:
+	if is_zero_approx(amount):
+		return
+		
+	head.rotate_object_local(Vector3.LEFT,deg_to_rad(amount))
+	head.orthonormalize()
+	
+func clamp_pitch()->void:
+	if head.rotation.x > deg_to_rad(min_pitch) and head.rotation.x < deg_to_rad(max_pitch):
+		return
+	head.rotation.x = clamp(head.rotation.x,deg_to_rad(min_pitch),deg_to_rad(max_pitch))
+	head.orthonormalize()
--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -1,22 +1,66 @@
 extends CharacterBody3D
-
-
+var damage = 0
+var elapsedTime = 0
+var wallRunTime = 0
+var touchedWall = false
+var jumped = false
+var wallLeft = false
+var wallRight = false
+const baseWallrunTime = 0.9
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
-
+@export var rayCastLeft: RayCast3D
+@export var rayCastRight: RayCast3D
+	
+func wall_on_side():
+	if rayCastLeft.is_colliding():
+		wallLeft = true
+	if rayCastRight.is_colliding():
+		wallRight = true
+	
+func wall_run_time(velocity):
+	var time = 0
+	time = abs(velocity) * baseWallrunTime
+	return time
+	
 func _physics_process(delta: float) -> void:
+	print(wallRight)
+	
+	elapsedTime += delta
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	#check for first touch with wall
+	if is_on_wall() and not is_on_floor() and not touchedWall:
+		touchedWall = true
+		wallRunTime = wall_run_time(velocity.x)
+		elapsedTime = 0
+		wall_on_side()
+	#jump from wall
+	if is_on_wall() and not is_on_floor() and Input.is_action_just_pressed("jump") and jumped == false:
+		velocity.y = JUMP_VELOCITY
+		jumped = true
+	#can wallrun while these are true	
+	if touchedWall == true and elapsedTime < wallRunTime and velocity.x > 0.5 and is_on_wall():
+		if Input.is_action_just_pressed("jump") and jumped == false:
+			velocity.y = JUMP_VELOCITY
+			jumped = true
+		else:
+			velocity.y = 0
+		
+	
+	if is_on_floor():
+		wallRight = false
+		wallLeft = false
+		touchedWall = false
+		jumped = false
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("moveLeft", "moveRight", "moveForwards", "moveBackwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
