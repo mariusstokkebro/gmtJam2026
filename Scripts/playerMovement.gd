a44ea139ab113491a029b2aa266b1b39b961975b
Marius Stokkebro <masto@itu.dk>
2026-07-26 12:48:58 +0200

    height also there

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -27,7 +27,7 @@ const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
-	height = position.y +999
+	height = (position.y * 2) + 999
 	apply_gravity(delta)	
 	match stateMachine.currentState:
 		stateMachine.playerState.IDLE:
